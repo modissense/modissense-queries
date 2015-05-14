8cecb1398507c87db692238150de11602e11d1b6
Giannis Giannakopoulos <ggian@cslab.ece.ntua.gr>
2015-05-14 23:20:51 +0300

    GetPOIClient -- added number of comments per POI

--- modissense-queries-clients/src/main/java/gr/ntua/ece/cslab/modissense/queries/clients/GetPOIClient.java
+++ modissense-queries-clients/src/main/java/gr/ntua/ece/cslab/modissense/queries/clients/GetPOIClient.java
@@ -5,12 +5,9 @@
  */
 package gr.ntua.ece.cslab.modissense.queries.clients;
 
-import gr.ntua.ece.cslab.modissense.queries.containers.UserIdStruct;
 import java.io.IOException;
-import java.nio.ByteBuffer;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.client.HTable;
-import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.ResultScanner;
 import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.util.Bytes;
@@ -20,48 +17,34 @@ import org.apache.hadoop.hbase.util.Bytes;
  * @author Giannis Giannakopoulos
  */
 public class GetPOIClient {
+    
+    private final String TABLE_NAME_TEXT_REPO="TextRepo";
 
     public GetPOIClient() {
     }
     
     
     /**
-     * Returns the number of comments (globally, for all users), for a poi based 
-     * 
-     * @param poiId
-     * @return 
+     * Returns the number of comments (globally, for all users) for a given poi.
+     * @param poiId 
+     * @return the number of comments made
+     * @throws java.io.IOException 
      */
     public int getNumberOfComments(Long poiId) throws IOException {
-        HTable table = new HTable(HBaseConfiguration.create(), "TextRepo");
-        byte[] startRow = new byte[Long.SIZE*2+1], stopRow=new byte[Long.SIZE*2+1];
-        UserIdStruct start = new UserIdStruct('F', Long.MIN_VALUE);
-        UserIdStruct stop = new UserIdStruct('t', Long.MAX_VALUE);
-        int index=0;
-        for (byte b:Bytes.toBytes(poiId)) {
-            startRow[index++] = b;
-        }
-        for (byte b:start.getBytes()) {
-            startRow[index++] = b;
-        }
-        index=0;
-        for (byte b:Bytes.toBytes(poiId)) {
-            stopRow[index++] = b;
-        }
-        for (byte b:stop.getBytes()) {
-            stopRow[index++] = b;
-        }
-        
-        Scan scan = new Scan(startRow, stopRow);
-        ResultScanner resScan= table.getScanner(scan);
-        int results = 0;
-        while(resScan.next()!=null) {
-            results++;
+        HTable table = new HTable(HBaseConfiguration.create(), TABLE_NAME_TEXT_REPO);
+        Scan scan = new Scan(Bytes.toBytes(poiId),Bytes.toBytes(poiId+1));
+        int results;
+        try (ResultScanner resScan = table.getScanner(scan)) {
+            results = 0;
+            while(resScan.next()!=null) {
+                results++;
+            }
         }
         return results;
     }
     
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         GetPOIClient client = new GetPOIClient();
         
         System.out.println("Results:" + client.getNumberOfComments(new Long(args[0])));
