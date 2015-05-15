323e3e5f5862949ce9022ae0276d39e78f78713c
Giannis Giannakopoulos <ggian@cslab.ece.ntua.gr>
2015-05-15 15:43:36 +0300

    committed

--- modissense-queries-clients/src/main/java/gr/ntua/ece/cslab/modissense/queries/clients/GetPOIClient.java
+++ modissense-queries-clients/src/main/java/gr/ntua/ece/cslab/modissense/queries/clients/GetPOIClient.java
@@ -5,12 +5,19 @@
  */
 package gr.ntua.ece.cslab.modissense.queries.clients;
 
+import gr.ntua.ece.cslab.modissense.queries.clients.containers.FriendsList;
+import gr.ntua.ece.cslab.modissense.queries.containers.ModissenseText;
 import gr.ntua.ece.cslab.modissense.queries.containers.UserIdStruct;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.client.HTable;
-import org.apache.hadoop.hbase.client.ResultScanner;
-import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.util.Bytes;
 
 /**
@@ -18,42 +25,136 @@ import org.apache.hadoop.hbase.util.Bytes;
  * @author Giannis Giannakopoulos
  */
 public class GetPOIClient {
-    
-    private final String TABLE_NAME_TEXT_REPO="TextRepo";
 
+    private final String 
+            TABLE_NAME_TEXT_REPO = "TextRepo",
+            TABLE_NAME_FRIENDS = "ModisUsers";
+
+    // fields used for input
+    private UserIdStruct userId;
+    private long poiId;
+
+    // fields used to carry output results
+    private int personalizedHotness;
+    private double personalizedInterest;
+    private String comment;
+    private String commentUser;
+    private String commentUserPicURL;
+    private int numberOfFriendsComments;
+
+    // intermediate friends list
+    private List<UserIdStruct> friendsList;
+
+    /**
+     * Empty constructor, does nothing by default.
+     */
     public GetPOIClient() {
     }
+
+    /**
+     * Constructor that initializes the
+     *
+     * @param userId
+     * @param poiId
+     */
+    public GetPOIClient(UserIdStruct userId, long poiId) {
+        this.userId = userId;
+        this.poiId = poiId;
+    }
     
     
+    
+    // public interface
+    public void executeQuery() throws IOException {
+        this.loadFriends();
+        this.parseFriendComments();
+    }
+
+    // util methods
+    private void loadFriends() throws IOException {
+        HTable table = new HTable(HBaseConfiguration.create(), TABLE_NAME_FRIENDS);
+        UserIdStruct qId = new UserIdStruct(this.userId.getC(), this.userId.getId());
+        Get get = new Get(qId.getBytes());
+        Result r = table.get(get);
+        for (Map.Entry<byte[], byte[]> kv : r.getFamilyMap("ids".getBytes()).entrySet()) {
+            FriendsList list = new FriendsList(qId);
+            list.parseCompressedBytes(kv.getValue());
+            this.friendsList = list.getFriends();
+        }
+        System.out.println("Friends loaded:"+this.friendsList);
+    }
+
     /**
      * Returns the number of comments (globally, for all users) for a given poi.
-     * @param poiId 
+     *
+     * @param poiId
      * @return the number of comments made
-     * @throws java.io.IOException 
+     * @throws java.io.IOException
      */
-    public int getNumberOfComments(Long poiId) throws IOException {
-        HTable table = new HTable(HBaseConfiguration.create(), TABLE_NAME_TEXT_REPO);
-        Scan scan = new Scan(Bytes.toBytes(poiId),Bytes.toBytes(poiId+1));
-        int results;
-        try (ResultScanner resScan = table.getScanner(scan)) {
-            results = 0;
-            while(resScan.next()!=null) {
-                results++;
-            }
+    private void parseFriendComments() throws IOException {
+        List<Get> getList = new LinkedList<>();
+        byte[] poiIdToBytes = Bytes.toBytes(this.poiId);
+        for(UserIdStruct u:this.friendsList) {
+            byte[] row = new byte[u.getBytes().length+poiIdToBytes.length];
+            int index=0;
+            for(byte b:poiIdToBytes)
+                row[index++]=b;
+            for(byte b:u.getBytes())
+                row[index++]=b;
+            getList.add(new Get(row));
         }
-        System.out.println("Number of global comments for POI "+poiId+": "+results);
-        return results;
-    }
-    
-    public String getCommentFromFriend(Long poiId, UserIdStruct friend){
-        return "";
-    }
-    
-    
-    public static void main(String[] args) throws IOException {
-        GetPOIClient client = new GetPOIClient();
+        HTable table = new HTable(HBaseConfiguration.create(), TABLE_NAME_TEXT_REPO);
+        Result[] results =table.get(getList);
+        this.numberOfFriendsComments=results.length;
         
-        System.out.println("Results:" + client.getNumberOfComments(new Long(args[0])));
+        List<ModissenseText> list = new LinkedList<>();
+        for(Result r: results) {
+            ModissenseText text = new ModissenseText();
+        }
     }
     
+    // Getters and setters
+    public void setUserId(UserIdStruct userId) {
+        this.userId = userId;
+    }
+
+    public void setPoiId(long poiId) {
+        this.poiId = poiId;
+    }
+
+    
+    // == 
+    public int getPersonalizedHotness() {
+        return personalizedHotness;
+    }
+
+    public double getPersonalizedInterest() {
+        return personalizedInterest;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public String getCommentUser() {
+        return commentUser;
+    }
+
+    public String getCommentUserPicURL() {
+        return commentUserPicURL;
+    }
+
+    public int getNumberOfFriendsComments() {
+        return numberOfFriendsComments;
+    }
+
+    public static void main(String[] args) throws IOException {
+        GetPOIClient client = new GetPOIClient(new UserIdStruct('F', 100008415518168L), 11534L);
+        
+        client.executeQuery();
+        System.out.println(client.getNumberOfFriendsComments());
+
+//        }
+    }
+
 }
---
+++ modissense-queries-clients/src/main/java/gr/ntua/ece/cslab/modissense/queries/clients/containers/FriendsList.java
@@ -0,0 +1,164 @@
+package gr.ntua.ece.cslab.modissense.queries.clients.containers;
+
+import gr.ntua.ece.cslab.modissense.queries.containers.UserIdStruct;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+import java.util.LinkedList;
+import java.util.zip.DataFormatException;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
+
+public class FriendsList {
+
+    private final byte[] key;
+    private LinkedList<UserIdStruct> friends;
+
+    public FriendsList(UserIdStruct user) {
+        key = user.getBytes();
+        friends = new LinkedList<>();
+    }
+
+    /**
+     *
+     * @return returns the current user's key
+     */
+    public byte[] getKey() {
+        return key;
+    }
+
+    /**
+     *
+     * @return returns a list with the keys of current user's friends, as they
+     * are represented in our HBase table
+     */
+    public LinkedList<UserIdStruct> getFriends() {
+        return friends;
+    }
+
+    /**
+     * friend list deserializer
+     *
+     * @param bytes
+     * @throws java.io.UnsupportedEncodingException
+     */
+    public void parseBytes(byte[] bytes) throws UnsupportedEncodingException {
+        int index = 0;
+
+        ByteBuffer buffer = ByteBuffer.wrap(bytes);
+
+        this.friends = new LinkedList<>();
+        int sizeOfFriendsList = buffer.getInt(index);
+        index += Integer.SIZE / 8;
+        UserIdStruct uid;
+        for (int i = 0; i < sizeOfFriendsList; i++) {
+            byte[] newFriend = new byte[this.key.length];
+            for (int j = 0; j < newFriend.length; j++) {
+                newFriend[j] = bytes[index + j];
+            }
+            index += this.key.length;
+            uid = new UserIdStruct();
+            uid.parseBytes(newFriend);
+            friends.add(uid);
+        }
+
+    }
+
+    /**
+     * friend list serializer
+     *
+     * @return
+     * @throws java.io.UnsupportedEncodingException
+     */
+    public byte[] getBytes() throws UnsupportedEncodingException {
+
+        int totalSize = Integer.SIZE / 8;
+
+        for (int i = 0; i < friends.size(); i++) {
+            totalSize += this.key.length;
+        }
+
+        byte[] serializable = new byte[totalSize];
+
+        ByteBuffer buffer = ByteBuffer.wrap(serializable);
+
+        buffer.putInt(friends.size());
+        for (UserIdStruct friendKey : friends) {
+            buffer.put(friendKey.getBytes());
+        }
+
+        return serializable;
+    }
+
+    public byte[] getCompressedBytes() {
+
+        byte[] serialization;
+        try {
+            serialization = this.getBytes();
+            Deflater deflater = new Deflater();
+            deflater.setLevel(4);
+            deflater.setInput(serialization);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            deflater.finish();
+            while (!deflater.finished()) {
+                int count = deflater.deflate(buffer);
+                stream.write(buffer, 0, count);
+            }
+
+            stream.close();
+            byte[] result = stream.toByteArray();
+            return result;
+        } catch (UnsupportedEncodingException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    public void parseCompressedBytes(byte[] array) {
+        Inflater inflater = new Inflater();
+        inflater.setInput(array);
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        while (!inflater.finished()) {
+            int count = 0;
+            try {
+                count = inflater.inflate(buffer);
+            } catch (DataFormatException e) {
+                e.printStackTrace();
+            }
+            stream.write(buffer, 0, count);
+        }
+        try {
+            stream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        byte[] decompressed = stream.toByteArray();
+        try {
+            this.parseBytes(decompressed);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer buffer = new StringBuffer();
+        
+//        buffer.append()
+        for(UserIdStruct f : this.friends) {
+            buffer.append(f.toString());
+            buffer.append(',');
+        }
+        return buffer.toString();
+    }
+    
+    
+}
